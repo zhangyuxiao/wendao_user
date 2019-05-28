ec8ed7550802c7365e8d0176d7ff475b1fad26a1
Your Name <you@example.com>
2019-05-28 13:46:16 +0800

    add package source files.

---
+++ src/GeekGhc/LaraFlash/Flash.php
@@ -0,0 +1,10 @@
+<?php
+namespace GeekGhc\LaraFlash;
+use Illuminate\Support\Facades\Facade;
+class Flash extends Facade
+{
+    public static function getFacadeAccessor()
+    {
+        return 'laraflash';
+    }
+}
